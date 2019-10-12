0d38e20b73d6a686aef44741e2e681a4d3621b24
Di3twater <dev.jonathan.green@gmail.com>
2019-10-12 19:21:03 -0400

    WIP

--- Services/TransacitonService.js
+++ Services/TransacitonService.js
@@ -1,6 +1,6 @@
 const ETH = require('eth-paymentchannel');
 const XRP = require('xrp-paymentchannel');
-
+const accountServices = require("./AccountServices")
 
 const getChannel = (destination, ledger, value, callback) => {
     //check if channel for destination exist
@@ -13,10 +13,37 @@ const getChannel = (destination, ledger, value, callback) => {
 }
 
 
-const newTransaction = (value, channel) => {
-
+const newTransaction = (ledger, value, channel) => {
+    const wallet//nodes wallet
+    switch (ledger) {
+        case "xrp":
+            const sig = XRP.sign(wallet, value, channel)
+            return sig
+            break;
+        case "eth":
+            const sig = ETH.sign(wallet, value, channel)
+            return sig
+            break;
+        default:
+        // code block
+    }
 }
 
-const verifyClaim = (AccountId, claim, value) => {
+const verifyClaim = (ledger, AccountId, sig, claim) => {
 
+    accountServices.findAccount(AccountId, (err, obj) => {
+        if (err) return console.error(err);
+        switch (ledger) {
+            case "xrp":
+                const isverified = XRP.verifyClaim(claim, sig, obj.address)
+                return isverified
+                break;
+            case "eth":
+                const isverified = ETH.verifyClaim(claim, sig, obj.address)
+                return isverified
+                break;
+            default:
+            // code block
+        }
+    })
 }
